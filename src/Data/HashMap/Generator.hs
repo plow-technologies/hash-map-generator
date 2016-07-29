ef73d1a4d9ebb981fd8705267810991a96f2d4bd
Robert Daro <mrrobbiedaro@gmail.com>
2016-07-29 14:33:34 -0500

    Awesome map done.

--- src/Data/HashMap/Generator.hs
+++ src/Data/HashMap/Generator.hs
@@ -1,27 +1,111 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE DefaultSignatures     #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
 module Data.HashMap.Generator where
 
+import Prelude hiding (lookup)
 import qualified Data.HashMap.Strict as H
-import Data.Hashable
-import Control.Lens
+import Data.Hashable 
+import GHC.Generics
+import qualified Data.Text as T
+import Language.Haskell.TH
+import Data.Proxy
+
+data One
+data Zero
+
 -- Requirements 
 -- Completely reified
 data AppMailboxes = LocationToEdit (Maybe Int)
                                     |  PartsToDisplay String
-   deriving (Eq, Ord, Show)
-
-instance Hashable AppMailboxes where
-    hashWithSalt i (LocationToEdit _) = hashWithSalt i "LocationToEdit"
-    hashWithSalt i (PartsToDisplay _) = hashWithSalt i "PartsToDisplay"
-
-keyLocationToEdit  = LocationToEdit Nothing
-keyPartsToDisplay = PartsToDisplay ""
-
-passableMailbox :: H.HashMap AppMailboxes AppMailboxes
-passableMailbox = H.fromList [(LocationToEdit  (Just 4), LocationToEdit (Just 3))
-                                                       , (PartsToDisplay "Brent", PartsToDisplay "Brent")
-                                                       ]
+   deriving (Eq, Ord, Show, Generic)
 
 
+-- keyLocationToEdit  = LocationToEdit Nothing
+-- keyPartsToDisplay = PartsToDisplay ""
 
--- submitEditIO :: BackofficeInterface -> STMMailbox (Maybe (Entity Location)) -> STMMailbox (S.Seq (PartByLocation)) -> STMMailbox Bool -> STMMailbox Text -> STMMailbox (S.Seq (Entity Part)) -> STMMailbox Text -> STMMailbox (Maybe Double) -> STMMailbox (Maybe Double) -> STMMailbox Bool -> STMMailbox (Maybe (Entity Part)) -> STMMailbox (H.HashMap PartId Bool) -> STMMailbox FormMode -> STMMailbox (Maybe (PartByLocation))  -> IO ()
--- submitEditIO interface mayLocToEditMb partsToDisplayMb partFormShowMb partFormSearchFieldMb dropDownPartsMb binNumberEntryMb binQtyEntryMb reservedQtyEntryMb partIsSelectedMb maySelectedPartMb hashMapEditValuesMb partFormModeMb mayPartToEditMb = do
+instance (GSelfHash rep) => GSelfHash (M1 i t rep) where
+    gSelfHashWithSalt i = gSelfHashWithSalt i . unM1
+    gSelfKey = gSelfKey .unM1
+
+instance SelfHash AppMailboxes where
+    
+
+safeHead d [] = d
+safeHead _ (x:_) = x
+
+instance GSelfHash (U1) where
+    gSelfHashWithSalt i _ = hashWithSalt i $ SelfKey ""
+    gSelfKey _ = SelfKey ""
+
+instance  (CN l, CN r) => GSelfHash (l :+: r) where
+  gSelfHashWithSalt i (L1 _) = hashWithSalt i .  SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy l)
+  gSelfHashWithSalt i (R1 _) = hashWithSalt i .  SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy r)
+  gSelfKey (L1 _) = SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy l)
+  gSelfKey (R1 _) = SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy r)
+
+newtype SelfKey = SelfKey {unSelfKey :: T.Text}
+  deriving (Eq, Ord, Show, Hashable)
+
+type SelfMap a = H.HashMap SelfKey a
+
+fromList :: (SelfHash a, Eq a) => [a] -> SelfMap a
+fromList v' = foldr mapMaker H.empty v'
+  where
+    mapMaker v m = H.insert (selfKey v) v m
+
+lookup :: (SelfHash a) => Name -> SelfMap a -> Maybe a
+lookup n m = H.lookup selfKey m
+  where
+    selfKey = SelfKey . T.pack . nameBase $ n
+
+insert :: (SelfHash a) =>  a -> SelfMap a -> SelfMap a
+insert v m = H.insert (selfKey v) v m
+
+-- ========================================================================================
+
+class CN (f :: * -> *) where
+ constructorNames :: proxy f -> [String]
+
+instance CN f => CN (D1 c f) where
+ constructorNames _ = constructorNames (Proxy :: Proxy f)
+
+instance (CN x, CN y) => CN (x :+: y) where
+ constructorNames _ = constructorNames (Proxy :: Proxy x) ++ constructorNames (Proxy :: Proxy y)
+
+instance Constructor c => CN (C1 c f) where
+ constructorNames _ = [conName (undefined :: t c f a)]
+
+class GSelfHash (f :: * -> *) where
+    gSelfHashWithSalt :: Int -> f a -> Int
+    gSelfKey :: f a -> SelfKey
+
+-- | Get the name of the constructor of a sum datatype.
+class GetConName f where
+    getConName :: f a -> String
+
+class SelfHash a where
+    selfHashWithSalt :: Int -> a -> Int
+    default selfHashWithSalt :: (Generic a, GSelfHash (Rep a)) => Int -> a -> Int
+    selfHashWithSalt = genericSelfHashWithSalt
+    selfKey :: a -> SelfKey
+    default selfKey :: (Generic a, GSelfHash (Rep a)) => a -> SelfKey
+    selfKey = genericSelfKey
+
+genericSelfHashWithSalt :: (Generic a, GSelfHash (Rep a)) => Int -> a -> Int
+genericSelfHashWithSalt i = gSelfHashWithSalt i .from
+
+genericSelfKey :: (Generic a, GSelfHash (Rep a)) => a -> SelfKey
+genericSelfKey = gSelfKey .from
