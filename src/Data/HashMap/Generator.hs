28f6048f0937e2c58529a0cca348be42b6b28164
Robert Daro <mrrobbiedaro@gmail.com>
2016-07-29 15:03:18 -0500

    Fixed warnings and organized code.

--- src/Data/HashMap/Generator.hs
+++ src/Data/HashMap/Generator.hs
@@ -23,8 +23,6 @@ import qualified Data.Text as T
 import Language.Haskell.TH
 import Data.Proxy
 
-data One
-data Zero
 
 -- Requirements 
 -- Completely reified
@@ -36,28 +34,12 @@ data AppMailboxes = LocationToEdit (Maybe Int)
 -- keyLocationToEdit  = LocationToEdit Nothing
 -- keyPartsToDisplay = PartsToDisplay ""
 
-instance (GSelfHash rep) => GSelfHash (M1 i t rep) where
-    gSelfHashWithSalt i = gSelfHashWithSalt i . unM1
-    gSelfKey = gSelfKey .unM1
+
 
 instance SelfHash AppMailboxes where
-    
 
-safeHead d [] = d
-safeHead _ (x:_) = x
-
-instance GSelfHash (U1) where
-    gSelfHashWithSalt i _ = hashWithSalt i $ SelfKey ""
-    gSelfKey _ = SelfKey ""
-
-instance  (CN l, CN r) => GSelfHash (l :+: r) where
-  gSelfHashWithSalt i (L1 _) = hashWithSalt i .  SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy l)
-  gSelfHashWithSalt i (R1 _) = hashWithSalt i .  SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy r)
-  gSelfKey (L1 _) = SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy l)
-  gSelfKey (R1 _) = SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy r)
-
-newtype SelfKey = SelfKey {unSelfKey :: T.Text}
-  deriving (Eq, Ord, Show, Hashable)
+-- ========================================================================================
+-- { Exported Things: }
 
 type SelfMap a = H.HashMap SelfKey a
 
@@ -67,34 +49,16 @@ fromList v' = foldr mapMaker H.empty v'
     mapMaker v m = H.insert (selfKey v) v m
 
 lookup :: (SelfHash a) => Name -> SelfMap a -> Maybe a
-lookup n m = H.lookup selfKey m
+lookup n m = H.lookup selfKey' m
   where
-    selfKey = SelfKey . T.pack . nameBase $ n
+    selfKey' = SelfKey . T.pack . nameBase $ n
 
 insert :: (SelfHash a) =>  a -> SelfMap a -> SelfMap a
 insert v m = H.insert (selfKey v) v m
 
--- ========================================================================================
-
-class CN (f :: * -> *) where
- constructorNames :: proxy f -> [String]
-
-instance CN f => CN (D1 c f) where
- constructorNames _ = constructorNames (Proxy :: Proxy f)
-
-instance (CN x, CN y) => CN (x :+: y) where
- constructorNames _ = constructorNames (Proxy :: Proxy x) ++ constructorNames (Proxy :: Proxy y)
-
-instance Constructor c => CN (C1 c f) where
- constructorNames _ = [conName (undefined :: t c f a)]
-
-class GSelfHash (f :: * -> *) where
-    gSelfHashWithSalt :: Int -> f a -> Int
-    gSelfKey :: f a -> SelfKey
-
--- | Get the name of the constructor of a sum datatype.
-class GetConName f where
-    getConName :: f a -> String
+-- ONLY EXPORT THE TYPE CONSTRUCTOR, NOT THE DATA CONSTRUCTOR
+newtype SelfKey = SelfKey {unSelfKey :: T.Text}
+  deriving (Eq, Ord, Show, Hashable)
 
 class SelfHash a where
     selfHashWithSalt :: Int -> a -> Int
@@ -103,9 +67,62 @@ class SelfHash a where
     selfKey :: a -> SelfKey
     default selfKey :: (Generic a, GSelfHash (Rep a)) => a -> SelfKey
     selfKey = genericSelfKey
+-- ========================================================================================
+
+-- ==========================================================================================
+-- { Non-exported instances: }
+
+instance (GSelfHash rep) => GSelfHash (M1 i t rep) where
+    gSelfHashWithSalt i = gSelfHashWithSalt i . unM1
+    gSelfKey = gSelfKey .unM1
+
+
+instance GSelfHash (U1) where
+    gSelfHashWithSalt i _ = hashWithSalt i $ SelfKey ""
+    gSelfKey _ = SelfKey ""
+
+-- Needed for safe head access
+safeHead :: forall t. t -> [t] -> t
+safeHead d [] = d
+safeHead _ (x:_) = x
+
+instance  (CN l, CN r) => GSelfHash (l :+: r) where
+   gSelfHashWithSalt i (L1 _) = hashWithSalt i .  SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy l)
+   gSelfHashWithSalt i (R1 _) = hashWithSalt i .  SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy r)
+   gSelfKey (L1 _) = SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy l)
+   gSelfKey (R1 _) = SelfKey . T.pack . safeHead "" . constructorNames $ (Proxy :: Proxy r)
+
+instance CN f => CN (D1 c f) where
+  constructorNames _ = constructorNames (Proxy :: Proxy f)
+
+instance (CN x, CN y) => CN (x :+: y) where
+  constructorNames _ = constructorNames (Proxy :: Proxy x) ++ constructorNames (Proxy :: Proxy y)
+
+instance Constructor c => CN (C1 c f) where
+  constructorNames _ = [conName (undefined :: t c f a)]
+-- ==========================================================================================
+
+-- ==========================================================================================
+-- { Miscellaneous Functions: }
 
 genericSelfHashWithSalt :: (Generic a, GSelfHash (Rep a)) => Int -> a -> Int
 genericSelfHashWithSalt i = gSelfHashWithSalt i .from
 
 genericSelfKey :: (Generic a, GSelfHash (Rep a)) => a -> SelfKey
 genericSelfKey = gSelfKey .from
+-- ==========================================================================================
+
+-- ==========================================================================================
+-- { Non-exported classes :}
+
+class CN (f :: * -> *) where
+ constructorNames :: proxy f -> [String]
+
+class GSelfHash (f :: * -> *) where
+    gSelfHashWithSalt :: Int -> f a -> Int
+    gSelfKey :: f a -> SelfKey
+
+-- | Get the name of the constructor of a sum datatype.
+class GetConName f where
+    getConName :: f a -> String
+-- ==========================================================================================
