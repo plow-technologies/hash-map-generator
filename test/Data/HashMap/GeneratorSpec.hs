b79659e94d26bf08431eacc0a5b00b0456035f0a
Scott Murphy <scottmurphy09@gmail.com>
2016-07-30 09:13:41 -0500

    tests all pass!
    Improved tests for correct hash creation, FAILING!
    
    Added alternate insert test
    
    Removed Proxy and CN type class
    
    tests all pass!

--- test/Data/HashMap/GeneratorSpec.hs
+++ test/Data/HashMap/GeneratorSpec.hs
@@ -9,6 +9,8 @@ import qualified Data.HashMap.Generator as G
 
 import           Test.Tasty
 
+import qualified Data.HashMap.Strict as HM
+
 import           Test.Tasty.HUnit
 
 import           GHC.Generics
@@ -19,6 +21,8 @@ import           Control.Lens
 -- | An example sum type
 data AppMailboxes = LocationToEdit (Maybe Int)
                 |  PartsToDisplay String
+                |  WondersToSeek  Int
+                |  LivesToLive    (Either String Int)
    deriving (Eq, Ord, Show, Generic)
 
 makePrisms ''AppMailboxes
@@ -27,9 +31,26 @@ instance SelfHash AppMailboxes where
 
 -- | Build a SelfMap similiarly to a regular Hashmap, however there is no Key
 -- field because that is what is generated.
+-- notice the order in expectedKeys doesn't match the order in the list
+-- That is because when it comes back from the list the hash order is different  
 exampleAppMailbox :: SelfMap AppMailboxes
-exampleAppMailbox = fromList [LocationToEdit (Just 1), PartsToDisplay "apart"]
+exampleAppMailbox = fromList [  LocationToEdit (Just 1)
+                            , PartsToDisplay "apart"
+                            , WondersToSeek 3
+                            , LivesToLive (Left "one life only")]
+                            
 
+exampleAppMailboxFromInserts :: SelfMap AppMailboxes
+exampleAppMailboxFromInserts = G.insert  (LocationToEdit (Just 1))           $
+                            G.insert (PartsToDisplay "apart")             $
+                            G.insert (WondersToSeek 3)                    $                            
+                            G.insert (LivesToLive (Left "one life only")) $ HM.empty
+
+expectedKeys :: [SelfKey AppMailboxes]
+expectedKeys =  [  key 'LocationToEdit
+                , key 'WondersToSeek
+                , key 'PartsToDisplay
+                , key 'LivesToLive]
 
 -- | Notice, to access the fields we use '\'' to grab the Template Haskell Name
 -- This means {-# LANGUAGE TemplateHaskell #-} is required
@@ -61,6 +82,12 @@ unitTests = testGroup "Unit tests"
 
   , testCase "Should retrive Part to Display" (
        lookupPartsToDisplay `compare` Just (PartsToDisplay "apart") @?= EQ)
+    
   , testCase "Should retrive Part to Display Using lens" (
-       useLensToLookupPartsToDisplay `compare` Just "apart" @?= EQ)       
+       useLensToLookupPartsToDisplay `compare` Just "apart" @?= EQ)
+
+  , testCase "keys example map should match expeted keys"  (
+       (HM.keys exampleAppMailbox) `compare` expectedKeys @?= EQ )
+  , testCase "keys example map from insertshould match expeted keys"  (
+       (HM.keys exampleAppMailboxFromInserts) `compare` expectedKeys @?= EQ )       
   ]
